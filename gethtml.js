5e7ea6f6e0746ea2c1447b1ac1c82a3fb2ccb2b2
lauratan <laura.tan108@gmail.com>
2018-06-11 18:58:25 +0000

    added gethtml

---
+++ gethtml.js
@@ -0,0 +1,26 @@
+var https = require('https');  
+
+function getAndPrintHTML () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+  
+    /* Add your code here */
+    https.get(requestOptions, function (response) {
+        response.setEncoding('utf8');
+
+        var result = "";
+
+        response.on('data', function (data) {
+            result += data;
+        });
+
+        response.on('end', function() {
+            console.log(result);
+        });
+    });
+};
+
+getAndPrintHTML();
