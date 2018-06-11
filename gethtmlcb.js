e895abc588787a10bdd9bd7adedf2f86c0f120b0
lauratan <laura.tan108@gmail.com>
2018-06-11 19:12:09 +0000

    added callbacks

---
+++ gethtmlcb.js
@@ -0,0 +1,29 @@
+var https = require('https');  
+
+function getHTML (options, callback) {
+
+    /* Add your code here */
+    https.get(requestOptions, function (response) {
+        response.setEncoding('utf8');
+        var result = "";
+        response.on('data', function (data) {
+            result += data;
+        });
+        response.on('end', function() {
+            callback(result);
+        });
+    });
+}
+
+function printHTML (html) {
+    console.log(html);
+  }
+  
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+};
+  
+    
+
+getHTML(requestOptions, printHTML);
